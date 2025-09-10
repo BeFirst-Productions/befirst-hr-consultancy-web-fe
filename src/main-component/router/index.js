05fec9a8c06312cdab4c1349a3b4a5bcdf06940d
arjunTltech <arjunntltechnologies@gmail.com>
2025-09-10 22:17:04 +0530

    fix: resolved hosting issue

--- src/main-component/router/index.js
+++ src/main-component/router/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import { Routes, Route, } from "react-router-dom";
-
-import Homepage from '../HomePage'
-import Homepage2 from '../HomePage2'
 import Homepage3 from '../HomePage3'
 import AboutPage from '../AboutPage'
 import ServicePage from '../ServicePage'
 import ServicePageS2 from '../ServicePageS2'
-import ServiceSinglePage from '../ServiceSinglePage/service1'
-// import ProjectPage from '../ProjectPage'
-// import ProjectPageS2 from '../ProjectPageS2'
-// import ProjectSinglePage from '../ProjectSinglePage'
-// import PricingPage from '../PricingPage'
-import TestimonialPage from '../TestimonialPage'
 import BlogPage from '../BlogPage'
-import BlogPageLeft from '../BlogPageLeft'
 import BlogPageFullwidth from '../BlogPageFullwidth'
 import BlogDetails from '../BlogDetails'
-import BlogDetailsLeftSiide from '../BlogDetailsLeftSiide'
 import BlogDetailsFull from '../BlogDetailsFull'
 import ErrorPage from '../ErrorPage'
 import ContactPage from '../ContactPage'
-// import LoginPage from '../LoginPage'
-// import SignUpPage from '../SignUpPage'
-// import ForgotPassword from '../ForgotPassword'
 import BlogSingle2 from '../../components/BlogDetails/blog2';
 import BlogSingle3 from '../../components/BlogDetails/blog3';
 import BlogSingle5 from '../../components/BlogDetails/blog5';
@@ -49,61 +35,64 @@ const AllRoute = () => {
 
   return (
     <div className="App">
-        <Routes>
-          <Route exact path='/' element={<Homepage3 />} />
-          <Route path='/about' element={<AboutPage/>} />
-          <Route path='/about/founders-vision' element={<FoundersVisionPage />} />
-          <Route path='/about/mission-&-values' element={<VisionAndMission />} />
+      <Routes>
+        <Route exact path='/' element={<Homepage3 />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/about/founders-vision' element={<FoundersVisionPage />} />
+        <Route path='/about/mission-&-values' element={<VisionAndMission />} />
 
-          <Route path='/services' element={<ServicePage/>} />
-          <Route path='/service-s2' element={<ServicePageS2/>} />
-          <Route path='/service/hr-operations-&-administration' element={<ServiceSinglePage1 />} />
-          <Route path='/service/hr-planning-&-organization' element={<ServiceSinglePage2 />} />
-          <Route path='/service/learning-&-development' element={<ServiceSinglePage3 />} />
-          <Route path='/service/payroll-&-compensation' element={<ServiceSinglePage4 />} />
-          <Route path='/service/recruitment-services' element={<ServiceSinglePage5 />} />
-          <Route path='/service/quality-assurance-&-compliance' element={<ServiceSinglePage6 />} />
-          <Route path='/service/talent-acquisition-&-Onboarding' element={<ServiceSinglePage7 />} />
+        <Route path='/services' element={<ServicePage />} />
+        <Route path='/service-s2' element={<ServicePageS2 />} />
+        <Route path='/service/hr-operations-&-administration' element={<ServiceSinglePage1 />} />
+        <Route path='/service/hr-planning-&-organization' element={<ServiceSinglePage2 />} />
+        <Route path='/service/learning-&-development' element={<ServiceSinglePage3 />} />
+        <Route path='/service/payroll-&-compensation' element={<ServiceSinglePage4 />} />
+        <Route path='/service/recruitment-services' element={<ServiceSinglePage5 />} />
+        <Route path='/service/quality-assurance-&-compliance' element={<ServiceSinglePage6 />} />
+        <Route path='/service/talent-acquisition-&-Onboarding' element={<ServiceSinglePage7 />} />
 
-          <Route path='/404' element={<ErrorPage />} />
-          <Route path='/contact' element={<ContactPage />} />
+        <Route path='/contact' element={<ContactPage />} />
 
-          <Route path='/blogs' element={<BlogPage />} />
-          <Route path='/blog-fullwidth' element={<BlogPageFullwidth />} />
-          <Route path='/blogs/blog1/blog-details' element={<BlogDetails />} />
-          <Route path='/blogs/blog2/blog-details' element={<BlogSingle2 />} />
-          <Route path='/blogs/blog3/blog-details' element={<BlogSingle3 />} />
-          <Route path='/blogs/blog4/blog-details' element={<BlogSingle4 />} />
-          <Route path='/blogs/blog5/blog-details' element={<BlogSingle5 />} />
-          <Route path='/blog-single-fullwidth' element={<BlogDetailsFull />} />
+        <Route path='/blogs' element={<BlogPage />} />
+        <Route path='/blog-fullwidth' element={<BlogPageFullwidth />} />
+        <Route path='/blogs/blog1/blog-details' element={<BlogDetails />} />
+        <Route path='/blogs/blog2/blog-details' element={<BlogSingle2 />} />
+        <Route path='/blogs/blog3/blog-details' element={<BlogSingle3 />} />
+        <Route path='/blogs/blog4/blog-details' element={<BlogSingle4 />} />
+        <Route path='/blogs/blog5/blog-details' element={<BlogSingle5 />} />
+        <Route path='/blog-single-fullwidth' element={<BlogDetailsFull />} />
 
-          <Route path='/careers' element={<CareersPage />} />
-          <Route path='/faqs' element={<FAQPage />} />
-          <Route path='/privacy-policy' element={<PrivacyPolicyPage />} />
-          <Route path='/terms-of-use' element={<TermsOfUsePage />} />
+        <Route path='/careers' element={<CareersPage />} />
+        <Route path='/careers/careers-form' element={<CareersFormPage />} />
 
-          <Route path='/careers/careers-form' element={<CareersFormPage />} />
+        <Route path='/faqs' element={<FAQPage />} />
+        <Route path='/privacy-policy' element={<PrivacyPolicyPage />} />
+        <Route path='/terms-of-use' element={<TermsOfUsePage />} />
 
-          {/* <Route path='/service-single/:id' element={<ServiceSinglePage />} /> */}
-          {/* <Route path='/testimonial' element={<TestimonialPage />} /> */}
-          {/* <Route path='/home' element={<Homepage/>} />
+        <Route path='/404' element={<ErrorPage />} />
+        <Route path='*' element={<ErrorPage />} />
+
+
+        {/* <Route path='/service-single/:id' element={<ServiceSinglePage />} /> */}
+        {/* <Route path='/testimonial' element={<TestimonialPage />} /> */}
+        {/* <Route path='/home' element={<Homepage/>} />
           <Route path='/home2' element={<Homepage2/>} /> */}
-          {/* <Route path='/home3' element={<Homepage3/>} /> */}
-          {/* <Route path='/service-single/' element={<ServiceSinglePage />} />
+        {/* <Route path='/home3' element={<Homepage3/>} /> */}
+        {/* <Route path='/service-single/' element={<ServiceSinglePage />} />
           <Route path='/service-single/' element={<ServiceSinglePage />} />
           <Route path='/service-single/' element={<ServiceSinglePage />} />
           <Route path='/service-single/' element={<ServiceSinglePage />} />
           <Route path='/service-single/' element={<ServiceSinglePage />} /> */}
-          {/* <Route path='/project' element={<ProjectPage />} />
+        {/* <Route path='/project' element={<ProjectPage />} />
           <Route path='/project-s2' element={<ProjectPageS2 />} />
           <Route path='/project-single' element={<ProjectSinglePage />} />
           <Route path='/pricing' element={<PricingPage />} /> */}
-          {/* <Route path='/blog-left-sidebar' element={<BlogPageLeft />} /> */}
-          {/* <Route path='/blog-single-left-sidebar' element={<BlogDetailsLeftSiide />} /> */}
-          {/* <Route path='/login' element={<LoginPage />} />
+        {/* <Route path='/blog-left-sidebar' element={<BlogPageLeft />} /> */}
+        {/* <Route path='/blog-single-left-sidebar' element={<BlogDetailsLeftSiide />} /> */}
+        {/* <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<SignUpPage />} />
           <Route path='/forgot-password' element={<ForgotPassword />} /> */}
-        </Routes>
+      </Routes>
       <HRChatbot />
 
     </div>
